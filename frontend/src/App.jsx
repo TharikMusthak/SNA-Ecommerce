84afd80685ee1f6082768e3907bcdb930601e27c
Tharik Musthak <tharikthoulath@gmail.com>
2026-08-14 13:46:23 +0530

    feat: update frontend source

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,9 @@ import './App.css'
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-green-600">
-    
+      <h1 className="text-5xl font-bold text-white">
+        SNA Sundaram
+      </h1>
     </div>
   )
 }
